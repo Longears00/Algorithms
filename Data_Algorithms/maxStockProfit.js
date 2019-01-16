d87004f818c507d9c224f33b8ead1628833dec84
yanghuan <yanghuan@laplacetech.cn>
2019-01-16 13:19:03 +0800

    .

--- Data_Algorithms/maxStockProfit.js
+++ Data_Algorithms/maxStockProfit.js
@@ -1,4 +1,4 @@
-function maxStockProfit(arr) {
+function maxStockProfit2(arr) {
 	var minElem = arr[0]
 	var maxElem = arr[0]
 	for (var i = 1; i < arr.length; i++) {
@@ -13,3 +13,31 @@ function maxStockProfit(arr) {
 }
 
 console.log(maxStockProfit([ 32, 46, 26, 38, 40, 48, 42 ]))
+
+function maxStockProfit(arr) {
+	var buyPrice = 0
+	var sellPrice = 0
+	var maxProfit = -1
+
+	var changeBuyPrice = true
+	for (var i = 1; i < arr.length; i++) {
+		if (changeBuyPrice) {
+			buyPrice = arr[i]
+		}
+		sellPrice = arr[i + 1]
+
+		if (sellPrice < buyPrice) {
+			changeBuyPrice = true
+		} else {
+			var tempProfit = sellPrice - buyPrice
+			if (tempProfit > maxProfit) {
+				maxProfit = tempProfit
+			}
+			changeBuyPrice = false
+		}
+	}
+
+	return maxProfit
+}
+
+console.log(maxStockProfit([ 10, 18, 4, 5, 9, 6, 16, 12 ]))
