504a385d8cd3265d6ed1d688a9394aed3c3e5ac4
yanghuan <yanghuan@laplacetech.cn>
2019-02-16 09:42:01 +0800

    .

--- Data_Algorithms/bubbleSort.js
+++ Data_Algorithms/bubbleSort.js
@@ -1,15 +1,24 @@
-function bubbleSort(arr) {
-	var len = arr.length
-	for (var i = 0; i < len; i++) {
-		for (var j = 1 + i; j < len; j++) {
-			if (arr[i] < arr[j]) {
-				var temp = arr[j]
-				arr[j] = arr[i]
-				arr[i] = temp
-			}
-		}
-	}
-	return arr
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort(array) {
+    let length = array.length
+    let count = length
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < count; j++) {
+            if (j + 1 === length) {
+                break
+            }
+            if (array[j] > array[j + 1]) {
+                let temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+
+        }
+        count--
+    }
+    return array
 }
 
-console.log(bubbleSort([ 2, 1, 3, 4, 8, 5 ]))
+bubbleSort(numbers);
+console.log(numbers);
---
+++ Data_Structures/Recursion.js
@@ -0,0 +1,17 @@
+// Write two functions that finds the factorial of any number. One should use recursive, the other should just use a for loop
+
+function findFactorialRecursive(number) {
+    //code here
+    if (number === 2) {
+        return 2
+    }
+    return number * findFactorialRecursive(number - 1);
+}
+
+function findFactorialIterative(number) {
+    let answer = 1
+    for (let i = 2; i <= number; i++) {
+        answer *= i
+    }
+    return answer;
+}
