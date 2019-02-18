10049e39d91288d4106b259e87075d05f3dcab66
yanghuan <yanghuan@laplacetech.cn>
2019-02-18 08:12:43 +0800

    .

--- Data_Structures/Binary_Tree.js
+++ Data_Structures/Binary_Tree.js
@@ -93,6 +93,79 @@ class BinaryTree {
             }
         }
     }
+
+    breathFirstSearch() {
+        let currentNode = this.root
+        let listResult = []
+        //keep trace
+        let queue = []
+        queue.push(currentNode)
+
+        while (queue.length > 0) {
+            currentNode = queue.shift()
+
+            listResult.push(currentNode.value)
+            if (currentNode.left) {
+                queue.push(currentNode.left)
+            }
+            if (currentNode.right) {
+
+                queue.push(currentNode.right)
+            }
+        }
+
+        console.log(listResult);
+    }
+    breathFirstSearchR(queue, list) {
+        if (queue.length === 0) {
+            return list
+        }
+        let currentNode = queue.shift()
+        listResult.push(currentNode.value)
+        if (currentNode.left) {
+            queue.push(currentNode.left)
+        }
+        if (currentNode.right) {
+            queue.push(currentNode.right)
+        }
+        return this.breathFirstSearchR(queue, list)
+    }
+    deepFirstSearchRInOrder(node, list) {
+        if (node.left) {
+            this.deepFirstSearchRInOrder(node.left, list)
+        }
+        list.push(node.value)
+        if (node.right) {
+            this.deepFirstSearchRInOrder(node.right, list)
+        }
+        return list
+    }
+
+    deepFirstSearchRPreOrder(node, list) {
+
+        list.push(node.value)
+        if (node.left) {
+            this.deepFirstSearchRPreOrder(node.left, list)
+        }
+        if (node.right) {
+            this.deepFirstSearchRPreOrder(node.right, list)
+        }
+        return list
+    }
+
+    deepFirstSearchRPostOrder(node, list) {
+
+
+        if (node.left) {
+            this.deepFirstSearchRPostOrder(node.left, list)
+        }
+        if (node.right) {
+            this.deepFirstSearchRPostOrder(node.right, list)
+        }
+        list.push(node.value)
+        return list
+    }
+
 }
 
 let tree = new BinaryTree()
@@ -103,7 +176,15 @@ tree.insert(20)
 tree.insert(170)
 tree.insert(15)
 tree.insert(1)
-console.log(JSON.stringify(traverse(tree.root)))
+//console.log(JSON.stringify(traverse(tree.root)))
+// tree.breathFirstSearch()
+//tree.breathFirstSearchR([tree.root], [])
+
+console.log(tree.deepFirstSearchRInOrder(tree.root, []))
+
+console.log(tree.deepFirstSearchRPreOrder(tree.root, []));
+
+console.log(tree.deepFirstSearchRPostOrder(tree.root, []));
 
 //     9
 //  4     20
